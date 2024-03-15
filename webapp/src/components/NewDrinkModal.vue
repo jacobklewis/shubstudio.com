65b0ec440b204232d55052c66c879110b596f6f9
jacobklewis <zerogamerfx@gmail.com>
2024-03-14 23:05:00 -0500

    basic ui set for drink calc

--- webapp/src/components/NewDrinkModal.vue
+++ webapp/src/components/NewDrinkModal.vue
@@ -7,7 +7,7 @@
 
       <q-card-section style="max-height: 80vh" class="scroll">
         <div class="text-h6">Volume (oz)</div>
-        <q-knob v-model="volume" size="20vw" :thickness="0.5" :step="0.5" :min="1" :max="16" color="orange" show-value
+        <q-knob v-model="volume" size="25vh" :thickness="0.5" :step="0.5" :min="1" :max="16" color="orange" show-value
           center-color="orange-1" class="q-ma-md" />
         <div class="text-h6">ABV</div>
         <q-btn-toggle v-model="abv" toggle-color="primary" spread :options="[
@@ -22,7 +22,7 @@
       </q-card-section>
 
       <q-card-actions align="right">
-        <q-btn flat label="Save" color="primary" v-close-popup />
+        <q-btn flat label="Save" @click="saveDrink" color="primary" />
         <q-btn flat label="Cancel" color="black" v-close-popup />
       </q-card-actions>
     </q-card>
@@ -31,9 +31,17 @@
 
 <script lang="ts" setup>
 
+import { api } from 'src/boot/axios';
 import { ref } from 'vue';
 
 const model = defineModel<boolean>();
 const volume = ref(12);
 const abv = ref(5);
+
+const saveDrink = () => {
+  api.post('apps/drinkcalc', { sizeOz: volume.value, abv: abv.value }).then((res) => {
+    console.log('saved drink!');
+    model.value = false
+  })
+}
 </script>
